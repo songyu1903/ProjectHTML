c67f24b45ad76d01513f8f7a95f61caae1c7529a
songyu1903 <k3s1234@naver.com>
2024-07-24 04:09:15 +0900

    project

---
+++ src/main/java/com/example/project/controller/mypage/MyPageController.java
@@ -0,0 +1,35 @@
+package com.example.project.controller.mypage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/myPage")
+public class MyPageController {
+    @GetMapping("setting")
+    public String setting() {
+        return "/mypage/setting";
+    }
+    @GetMapping("myDebate")
+    public String myDebate() {
+        return "mypage/myDebate";
+    }
+
+    @GetMapping("myBook")
+    public String myBook(){
+        return "mypage/myBook";
+    }
+    @GetMapping("myPost")
+    public String myPost(){
+        return "mypage/myPost";
+    }
+
+    @PostMapping("myBook")
+    public String myBookPost(){
+        return "redirect:/mypage/setting";
+    }
+}
---
+++ src/test/java/com/example/project/controller/mypage/MyPageControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.project.controller.mypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MyPageControllerTest {
+
+}
