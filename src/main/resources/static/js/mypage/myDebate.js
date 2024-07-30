258dc7b6b28611f82c3ce396854135459162d7b9
songyu1903 <k3s1234@naver.com>
2024-07-30 18:50:43 +0900

    project

--- src/main/resources/static/js/mypage/myDebate.js
+++ src/main/resources/static/js/mypage/myDebate.js
@@ -1,23 +1,24 @@
-    function openTab(tabName) {
+function openTab(tabName) {
     // 모든 탭 컨텐츠를 숨깁니다.
-    const tabContents = document.querySelectorAll('.tab-content');
+    const tabContents = document.querySelectorAll('.body_tab');
     tabContents.forEach(tabContent => {
-    tabContent.classList.remove('active');
-    tabContent.setAttribute('aria-hidden', 'true');
-});
+        tabContent.classList.remove('body_tab--active');
+        tabContent.setAttribute('aria-hidden', 'true')
+    });
+
 
     // 모든 탭을 비활성화합니다.
-    const tabs = document.querySelectorAll('.my-room__body__tab');
+    const tabs = document.querySelectorAll('.body_tab');
     tabs.forEach(tab => {
-    tab.classList.remove('my-room__body__tab--active');
-});
+        tab.classList.remove('active');
+    });
 
     // 선택한 탭 컨텐츠를 표시합니다.
     const activeTabContent = document.getElementById(tabName);
     activeTabContent.classList.add('active');
-    activeTabContent.setAttribute('aria-hidden', 'false');
+    activeTabContent.setAttribute('aria-hidden' , 'false');
 
     // 선택한 탭을 활성화합니다.
-    const activeTab = Array.from(tabs).find(tab => tab.textContent.includes(tabName === 'active' ? '진행 중' : '모집 중'));
-    activeTab.classList.add('my-room__body__tab--active');
+    const activeTab = document.getElementById('tab-' + tabName);
+    activeTab.classList.add('active');
 }
